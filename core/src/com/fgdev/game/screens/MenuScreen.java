7f651ffe5dac1239e5d4721d8408696329ba5968
Duong Minh Hoang <angelkittybihoang@gmail.com>
2018-12-10 15:47:40 +0700

    Screen Transaction

--- core/src/com/fgdev/game/screens/MenuScreen.java
+++ core/src/com/fgdev/game/screens/MenuScreen.java
@@ -2,6 +2,7 @@ package com.fgdev.game.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -10,6 +11,8 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.fgdev.game.screens.transitions.ScreenTransition;
+import com.fgdev.game.screens.transitions.ScreenTransitionFade;
 import com.fgdev.game.util.Constants;
 import com.fgdev.game.util.GamePreferences;
 
@@ -42,7 +45,7 @@ public class MenuScreen extends AbstractGameScreen {
     private boolean debugEnabled = true;
     private float debugRebuildStage;
 
-    public MenuScreen (Game game) {
+    public MenuScreen (DirectedGame game) {
         super(game);
     }
 
@@ -90,7 +93,6 @@ public class MenuScreen extends AbstractGameScreen {
     public void show() {
         stage = new Stage(new StretchViewport(Constants.VIEWPORT_GUI_WIDTH,
                 Constants.VIEWPORT_GUI_HEIGHT));
-        Gdx.input.setInputProcessor(stage);
         rebuildStage();
     }
 
@@ -104,6 +106,11 @@ public class MenuScreen extends AbstractGameScreen {
     @Override
     public void pause() {
 
+    }
+
+    @Override
+    public InputProcessor getInputProcessor () {
+        return stage;
     }
 
     private Table buildBackgroundLayer () {
@@ -215,7 +222,8 @@ public class MenuScreen extends AbstractGameScreen {
     }
 
     private void onPlayClicked () {
-        game.setScreen(new GameScreen(game));
+        ScreenTransition transition = ScreenTransitionFade.init(0.75f);
+        game.setScreen(new GameScreen(game), transition);
     }
 
     private void onOptionsClicked () {
